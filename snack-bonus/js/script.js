87314daec55f2ad5b4ac87a2b5f4a961704888d9
filippo-falcone <falconefilippo98@hotmail.com>
2024-03-12 19:02:07 +0100

    feat: add snack bonus functionality

--- snack-bonus/js/script.js
+++ snack-bonus/js/script.js
@@ -1,3 +1,20 @@
 /* --------------------------------------------------------------------------------------------------------
    Chiedi un numero di 4 cifre all’utente e calcola la somma di tutte le cifre che compongono il numero.
 -------------------------------------------------------------------------------------------------------- */
+/*
+// Chiedo all'utente un numero composto da 4 cifre
+// Creo un ciclo for
+// - Converto gli indici in numero
+// - Sommo i numeri
+// Stampo la somma in un allert
+*/
+// Variabili
+const userNumber = prompt('Inserisci un numero di 4 cifre');
+// Funzionalità
+let sum = 0;
+for (let i = 0; i < userNumber.length; i++) {
+    const thisNumber = parseInt(userNumber[i]);
+    console.log(thisNumber);
+    sum += thisNumber;
+}
+alert(sum);
