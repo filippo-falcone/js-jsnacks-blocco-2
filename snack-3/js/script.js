d07c2a52c64a0bd9da52c052f11b2eae9bdaadb3
filippo-falcone <falconefilippo98@hotmail.com>
2024-03-12 18:23:58 +0100

    feat: add snack 3 functionality

--- snack-3/js/script.js
+++ snack-3/js/script.js
@@ -3,3 +3,25 @@
     - un div avrà il testo colorato di rosso mentre l’altro di verde.
     - Partendo da un array di numeri, stampiamo nell’id rosso i numeri dispari e in verde i numeri pari.
 -------------------------------------------------------------------------------------------------------- */
+/*
+// Richiamo il container in cui andranno i div
+// Creo un array
+// Scorro l'array per controllare se i numeri sono pari o dispari
+// - Se pari creo un div con id rosso e stampo
+// - Altrimenti creo un div con id verde e stampo in numero
+*/
+// Variabili
+const textContainer = document.querySelector(".text-container");
+const numberList = [2, 4, 5, 12, 34, 78, 90, 12, 33, 56, 223, 77, 1, 49, 12];
+// Funzionalità
+for (let i = 0; i < numberList.length; i++) {
+    const thisNumber = numberList[i];
+    if (numberList[i] % 2 == 0) {
+        textContainer.innerHTML += `<div id="red">${thisNumber}</div>`;
+    } else {
+        const greenDiv = document.createElement("div");
+        greenDiv.setAttribute("id", "green");
+        greenDiv.innerHTML = `${thisNumber}`;
+        textContainer.append(greenDiv);
+    }
+}
